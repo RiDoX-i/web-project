3b991ab26d8536de28f357fa88c5fa5a43cad4d5
Your Name <abdelkrim.hadjmohand@gmail.com>
2025-04-04 21:42:50 +0200

    changed

--- HF/header.php
+++ HF/header.php
@@ -1,36 +1,27 @@
 <?php 
     session_start();
 ?>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark py-4">
+  <div class="container">
+    <a class="navbar-brand d-flex align-items-center" href="./web-project/main-menu/home.php">
+      <img src="/web-project/HF/logo.png" alt="Logo" width="40" height="40" class="me-2">
+      <h2 class="m-0 fw-light">Sportify</h2>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mx-auto">
+        <li class="nav-item"><a class="nav-link" href="./web-project/main-menu/home.php">Accueil</a></li>
+        <li class="nav-item"><a class="nav-link" href="cours.php">Nos Cours</a></li>
+        <li class="nav-item"><a class="nav-link" href="devis.php">Demande de Devis</a></li>
+        <li class="nav-item"><a class="nav-link" href="./web-project/contact/index.php">Contact</a></li>
+      </ul>
+      <?php if (!$_SESSION['login_success']): ?>
+        <a href="login.html" class="nav-link">Connexion | Inscription</a>
+      <?php endif; ?>
+    </div>
+  </div>
+</nav>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-<header class="bg-dark text-white py-4"> <!-- Hauteur augmentée -->
-        <div class="container">
-            <div class="d-flex justify-content-between align-items-center">
-                
-                <!-- Logo (plus à gauche) -->
-                <div class="d-flex align-items-center logo-container">
-                <img src="/web-project/HF/logo.png" alt="Logo" width="40" height="40" class="me-2">
-                <h2 class="m-0 fw-light">Sportify</h2> <!-- fw-light pour une écriture fine -->
-                </div>
-
-                <!-- Connexion (texte simple, plus haut) -->
-                <?php if (!$_SESSION['login_success']): ?>
-                
-                    <div class="login">
-                     <a href="login.html" class="nav-link">Connexion | Inscription</a>
-                    </div>
-
-                <?php endif; ?>
-
-            </div>
-
-            <!-- Liens de navigation (centrés et plus bas) -->
-            <nav class="mt-3">
-                <ul class="nav justify-content-center">
-                    <li class="nav-item"><a class="nav-link" href="./web-project/main-menu/home.php">Accueil</a></li>
-                    <li class="nav-item"><a class="nav-link" href="cours.php">Nos Cours</a></li>
-                    <li class="nav-item"><a class="nav-link" href="devis.php">Demande de Devis</a></li>
-                    <li class="nav-item"><a class="nav-link" href="./web-project/contact/index.php">Contact</a></li>
-                </ul>
-            </nav>
-        </div>
-</header>
