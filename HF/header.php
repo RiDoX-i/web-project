0a20da80cc3a1fcf881d7178b2d2ec247585b5e6
Your Name <abdelkrim.hadjmohand@gmail.com>
2025-04-04 22:34:29 +0200

    update

--- HF/header.php
+++ HF/header.php
@@ -1,9 +1,10 @@
 <?php 
     session_start();
 ?>
+
 <nav class="navbar navbar-expand-md navbar-dark bg-dark py-2">
   <div class="container">
-    <a class="navbar-brand d-flex align-items-center" href="./web-project/main-menu/home.php">
+    <a class="navbar-brand d-flex align-items-center" href="../main-menu/home.php">
       <img src="/web-project/HF/logo.png" alt="Logo" width="40" height="40" class="me-2">
       <h2 class="m-0 fw-light fs-4">Sportify</h2>
     </a>
@@ -13,20 +14,20 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav mx-auto">
         <li class="nav-item mx-5">
-          <a class="nav-link fs-5" href="./web-project/main-menu/home.php">Accueil</a>
+          <a class="nav-link fs-5" href="../main-menu/home.php">Accueil</a>
         </li>
         <li class="nav-item mx-5">
-          <a class="nav-link fs-5" href="cours.php">Nos Cours</a>
+          <a class="nav-link fs-5" href="../activities/activities.php">Nos Cours</a>
         </li>
         <li class="nav-item mx-5">
-          <a class="nav-link fs-5" href="devis.php">Demande de Devis</a>
+          <a class="nav-link fs-5" href="../devis/devis.php">Demande de Devis</a>
         </li>
         <li class="nav-item mx-5">
-          <a class="nav-link fs-5" href="./web-project/contact/index.php">Contact</a>
+          <a class="nav-link fs-5" href="../contact/index.php">Contact</a>
         </li>
       </ul>
       <?php if ($_SESSION['login_success']): ?>
-        <a href="login.html" class="nav-link fs-5">Connexion | Inscription</a>
+        <a href="../connexion/connexion.php" class="nav-link fs-5">Connexion | Inscription</a>
       <?php endif; ?>
     </div>
   </div>
--- activities/activities.php
+++ activities/activities.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="./bootstrap.css">
     <link rel="stylesheet" href="./activities.css">
+    <link rel="stylesheet" href="../HF/index.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Activities</title>
 </head>
