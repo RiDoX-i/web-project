6af82b8009f50e06e557006724ff9dd1c70ba9ae
precieux-mg <gloireprcx@gmail.com>
2025-04-06 21:19:14 +0200

    recap courses

--- activities/courses-report/courses-report.php
+++ activities/courses-report/courses-report.php
@@ -4,21 +4,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="courses-report.css">
+
     <title>courses-report</title>
 </head>
 </p>
 <body>
        <main>
-            <div id="couses-report-container">
-                <p>
-                    Nom : <br>
-                    Prénom : <br>
-                    Cours réservé: <br>
-                    Durée du cours : <br>
-                    Encadreur: <br>
-                    Date du cours : <br>
-                </p>
+            <div class="d-flex justify-content-center align-items-center vh-100">
+                <div class="p-4 bg-light border rounded" id="courses-report-container">
+                    <h2>Récapitulatif de votre inscription</h2>
+                    <p>
+                        Nom :<br>
+                        Prénom : <br>
+                        Cours réservé: <br>
+                        Durée du cours : <br>
+                        Encadreur: <br>
+                        Date du cours : <br>
+                    </p>
+                </div>
             </div>
        </main>
 </body>
