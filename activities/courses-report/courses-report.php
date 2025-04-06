d041c1ec66d0b2a65b343542e41d48ab1fa7fdc0
precieux-mg <gloireprcx@gmail.com>
2025-04-06 20:42:53 +0200

    courses report page

---
+++ activities/courses-report/courses-report.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./courses-report.css">
+    <title>courses-report</title>
+</head>
+<body>
+       <main>
+            <div id="couses-report-container">
+                <p>
+                    Nom : <br>
+                    Prénom : <br>
+                    Cours réservé: <br>
+                    Durée du cours : <br>
+                    Encadreur: <br>
+                    Date du cours : <br>
+                </p>
+            </div>
+       </main>
+</body>
+</html>
