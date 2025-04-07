009212c3b8d509fa4be1a8af0d29b8a6e07dbd32
RiDoX-i <abdelkrim.hadjmohand@gmail.com>
2025-04-07 17:04:59 +0200

    fixed to the left side connecting and disconnect and user name

--- HF/header.php
+++ HF/header.php
@@ -1,66 +1,50 @@
 <?php 
-    session_start();
+session_start();
 ?>
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-
-
-  <!-- logo and name -->
-  <a class="navbar-brand" href="#">
-  <img src="/web-project/HF/logo.png" alt="Logo" width="40" height="40" class="me-2">  
-  Sportify</a>
-
-  <!-- hambergur-->
-  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <!-- elemnts -->
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-
-      
-      <li class="nav-item active">
-        <a class="nav-link" href="../main-menu/home.php">Accueil</a>
-      </li>
-
-
-      <li class="nav-item">
-        <a class="nav-link" href="../activities/activities.php">Nos Cours</a>
-      </li>
-
-
-      <li class="nav-item">
-        <a class="nav-link" href="../devis/devis.php">Demande de Devis</a>
-      </li>
-
-
-
-      <li class="nav-item">
-        <a class="nav-link" href="../contact/contact.php">Contact</a>
-      </li>
-
-
-    <!-- Only show username if `login_success` is set and true -->
-    <li class="nav-item">
+  <div class="container-fluid">
+    <!-- Logo and Site Name -->
+    <a class="navbar-brand" href="#">
+      <img src="/web-project/HF/logo.png" alt="Logo" width="40" height="40" class="me-2">  
+      Sportify
+    </a>
+    <!-- Hamburger Menu -->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Navigation Links -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <!-- Left Side Navigation -->
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="../main-menu/home.php">Accueil</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../activities/activities.php">Nos Cours</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../devis/devis.php">Demande de Devis</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="../contact/contact.php">Contact</a>
+        </li>
+      </ul>
+      <!-- Right Side Navigation (Username and Login/Logout) -->
+      <ul class="navbar-nav ms-auto">
         <?php if (isset($_SESSION['username'])): ?>  
-          <a class="nav-link" href="#" >
-          <?= $_SESSION['username'] ?>
-          </a>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><?= $_SESSION['username'] ?></a>
+          </li>
         <?php endif; ?>
-    </li>
-
-    <li class="nav-item">
-  <?php if (!isset($_SESSION['login_success']) || !$_SESSION['login_success']): ?>
-    <a href="../connexion/connexion.php" class="nav-link">Connexion | Inscription</a>
-  <?php else: ?>
-    <a href="../connexion/disconnect.php" class="nav-link">Se Déconnecter</a>
-  <?php endif; ?>
-</li>
-
- 
-
-    </ul>
-    
+        <li class="nav-item">
+          <?php if (!isset($_SESSION['login_success']) || !$_SESSION['login_success']): ?>
+            <a href="../connexion/connexion.php" class="nav-link">Connexion | Inscription</a>
+          <?php else: ?>
+            <a href="../connexion/disconnect.php" class="nav-link">Se Déconnecter</a>
+          <?php endif; ?>
+        </li>
+      </ul>
+    </div>
   </div>
 </nav>
