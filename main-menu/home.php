2ff7a082d408c9e51cab630f4c8d16eb4b0bc956
precieux-mg <gloireprcx@gmail.com>
2025-04-10 22:42:00 +0200

    fix code vadator

--- main-menu/home.php
+++ main-menu/home.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +9,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;700&display=swap" rel="stylesheet">
   <link href="../HF/HF.css" rel="stylesheet">
 
-  <title>Home page</title>
+  <title>Accueil</title>
 </head>
 <body>
   <header>
