210c287b36e73377588aebe13442b5b4c1fa5b38
precieux-mg <gloireprcx@gmail.com>
2025-04-11 15:55:53 +0200

    recap update

--- recap/recap.php
+++ recap/recap.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +10,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../HF/HF.css">
 
-    <title>courses-report</title>
+    <title>Récapitulatif d'inscription</title>
 </head>
 </p>
 <body>
@@ -29,6 +29,10 @@
                      <p>Niveau :  <?=$_SESSION['lvl']?></p>
                     <?php endif; ?>
                     <p>Date de réservation : <?=$_SESSION['date_res']?></p>
+                    <p>Merci de votre confiance en nous !</p>
+                    <div class="d-flex justify-content-center align-items-center" id="retour">
+                        <a href="../activities/activities.php" class="btn  w-25">Retour</a>
+                    </div>
             </div>
         </div>
     </main>
