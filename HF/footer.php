9c3acbe090221826efe075f21f54cd09be10ad72
RiDoX-i <abdelkrim.hadjmohand@gmail.com>
2025-04-13 18:02:34 +0200

    .

--- HF/footer.php
+++ HF/footer.php
@@ -3,8 +3,8 @@
     <address class="mb-3">
       <p class="mb-1">185 Avenue Albert Thomas, 87000 Limoges</p>
       <p class="mb-1">
-        <a href="mailto:contact@sportify.com" class="text-white text-decoration-none">
-          contact@sportify.com
+        <a href="mailto:sportify@mail.com" class="text-white text-decoration-none">
+          sportify@mail.com
         </a>
       </p>
       <p class="mb-0">+33 6 12 34 56 78</p>
