499a8c2ef2be0b4411ec219c2942ec5a492532e0
RiDoX-i <abdelkrim.hadjmohand@gmail.com>
2025-04-13 17:49:04 +0200

    bts

--- connexion/connexion.php
+++ connexion/connexion.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title>Connexion et Inscription</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./bootstrap.css">
+    <link rel="stylesheet" href="../bootstrap.css">
     <link rel="stylesheet" href="../HF/HF.css">
     <link rel="stylesheet" href="connexion.css">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet">
